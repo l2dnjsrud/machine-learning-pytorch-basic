66c555ded94b073189f2e06e47a15252fe47794c
Wonkyung Lee <hjtwin2@gmail.com>
2022-01-28 12:45:16 +0900

    Create fullcode.py

---
+++ ch03/fullcode.py
@@ -0,0 +1,20 @@
+nb_epochs = 20
+for epoch in range(nb_epochs + 1):
+    for batch_idx, samples in enumerate(dataloader):
+        x_train, y_train = samples
+
+        # H(x) 계산
+        prediction = model(x_train)
+
+        # cost 계산
+        cost = F.mse_loss(prediction, y_train)
+
+        # cost로 H(x) 개선
+        optimizer.zero_grad()
+        cost.backward()
+        optimizer.step()
+
+        print('Epoch {:4d}/{} Batch {}/{} Cost: {:.6f}'.format(
+            epoch, nb_epochs, batch_idx+1, len(dataloader),
+            cost.item()
+ ))
